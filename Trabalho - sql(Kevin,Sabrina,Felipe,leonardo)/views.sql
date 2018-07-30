7da454006b7222bc2113897b5d4fd55bc43e7a27
KevinRSoares <kevin_soares@estudante.sc.senai.br>
2018-07-29 23:33:11 -0300

    Adicionado o arquivo do Trabalho de sql;

---
+++ Trabalho - sql(Kevin,Sabrina,Felipe,leonardo)/views.sql
@@ -0,0 +1,12 @@
+--Views
+	--Views Ranking Geral
+		Create View RankingGeral as 
+		Select NicUsu as 'NickName',max(PonPar) as 'Pontuação' from Usuario
+		right Join partida using(CodUsu)
+		group by CodUsu order by max(PonPar) desc limit 10;
+	--Views Ranking Geral	
+	--Views Relatório Log
+		Create View relatorio_log as 
+		Select Deslog as 'Descrição Log', DatHorLog as 'Data e hora do Log', TipLog as 'Tipo do Log' from log;	
+	--Views Relatório Log
+--Views
