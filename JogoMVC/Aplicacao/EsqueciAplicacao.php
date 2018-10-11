a07b3b5b6152594bda467d049c845f2666ce1386
KevinRSoares <kevin_soares@estudante.sc.senai.br>
2018-10-11 00:15:09 -0300

    Alterações Sabrina

--- JogoMVC/Aplicacao/EsqueciAplicacao.php
+++ JogoMVC/Aplicacao/EsqueciAplicacao.php
@@ -15,18 +15,18 @@ class EsqueciAplicacao{
 		if (empty($Esqueci->EmailE)) {
 			$erros['email'] = 'Você deve informar um email';
 		}
-    	if (!empty($erros)) { 
+		if (!empty($erros)) { 
 			$form_data['success'] = false;
 			$form_data['erros']  = $erros;
 		}
 		else { 
-	
+			
 			$email = $Esqueci->EmailE;
-	
+			
 			$sql = "SELECT * FROM Usuario Where EmaUsu = ?";
 			$stmt = $conn->prepare($sql);
 			$stmt->bind_param('s', $email); 
-	
+			
 			$stmt->execute();
 			if ($stmt->error) {
 				$erros['nome'] = "Erro: " . $conn->error;
@@ -37,56 +37,74 @@ class EsqueciAplicacao{
 				$result = $stmt->get_result();
 				if ($result->num_rows > 0) {
 					if($row = $result->fetch_assoc()) {
-	
-						
+						$Codigo = $row["CodUsu"];	
 						$Nome	= $row["NomUsu"];	
-						$Email	= $row["EmaUsu"];	
-						$Senha  = $row["SenUsu"];
-	
+						$Email	= $row["EmaUsu"];
+						$Senha = '';	
+						$num = 0;
+						for( $i= 0; $i < 4;$i++){
+							$num = rand (1 , 9);
+							$Senha = $Senha.(string)$num;
+						}
+						$this->Atualiza_Senha($Codigo,$Senha);
 						$Recuperacao = "Nome: $Nome<br>E-mail: $Email<br>Senha: $Senha";
-	
+						
 						require_once('../phpmailer/class.phpmailer.php');
-				
-		$mail = new PHPMailer();
-		
-		$mail->IsSMTP();
+						
+						$mail = new PHPMailer();
+						
+						$mail->IsSMTP();
 
-		$mail->CharSet = 'UTF-8';
-		$mail->Port = '465'; // Portal SSL Gmail
-		$mail->Host = 'smtp.gmail.com'; 
-		$mail->IsHTML(true); 
-		$mail->Mailer = 'smtp'; 
-		$mail->SMTPSecure = 'ssl';
+						$mail->CharSet = 'UTF-8';
+						$mail->Port = '465'; // Portal SSL Gmail
+						$mail->Host = 'smtp.gmail.com'; 
+						$mail->IsHTML(true); 
+						$mail->Mailer = 'smtp'; 
+						$mail->SMTPSecure = 'ssl';
 
-		$mail->SMTPAuth = true; 
-		$mail->Username = 'fastmnd@gmail.com'; // Email do Usuário
-		$mail->Password = 'fastmind20'; // Senha do Gmail
+						$mail->SMTPAuth = true; 
+						$mail->Username = 'fastmnd@gmail.com'; // Email do Usuário
+						$mail->Password = 'fastmind20'; // Senha do Gmail
 
-		$mail->SingleTo = true; 
+						$mail->SingleTo = true; 
 
 
-		$mail->From = 'fastmnd@gmail.com'; 
-		$mail->FromName = "JOGO - Recuperação de Senha";
+						$mail->From = 'fastmnd@gmail.com'; 
+						$mail->FromName = "JOGO - Recuperação de Senha";
 
-		$mail->addAddress($Email);
+						$mail->addAddress($Email);
 
-		$mail->Subject = "Recuperação de Senha"; 
-		$mail->Body = $Recuperacao;
+						$mail->Subject = "Recuperação de Senha"; 
+						$mail->Body = $Recuperacao;
 
-		if(!$mail->Send()){
-			echo "Erro ao enviar Email: " . $mail->ErrorInfo;
-		}
+						if(!$mail->Send()){
+							echo "Erro ao enviar Email: " . $mail->ErrorInfo;
+						}
 
-	
-	}
-	
-	}
-		}
-	
+						
+					}
+					
+				}
+			}
+			
 			$conn->close();		
 		}
 
 	}
+	public function Atualiza_Senha($Usuario,$SenUsu)
+	{		
+		$connection = new Connection();
+		$conn = $connection->getConn();
+		$stmt = $conn->prepare("call esqueci_senha(?,?)");
+		//i   corresponding variable has type integer
+		//d   corresponding variable has type double
+		//s   corresponding variable has type string
+		//b   corresponding variable is a blob and will be sent in packets
+		$stmt->bind_param('is', $Usuario, $SenUsu);
+		//NoUsu EmUsu DtNaUsu NiUsu SeUsu TiUsu 
+		// 's' especifica o tipo => 'string'
+		$stmt->execute();
+		$conn->close();		
+	}	
 }
-
 ?>
